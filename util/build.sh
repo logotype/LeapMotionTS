185a8512d3299468c82a45a30bbfad7f915c7002
Victor Norgren <victor@logotype.se>
2013-05-22 21:58:30 +0800

    added declaration file

--- util/build.sh
+++ util/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd ${0%/*}
 echo "[Combining modules...]"
 cat ../modules/util/EventDispatcher.ts ../modules/Listener.ts ../modules/interfaces/DefaultListener.ts ../modules/util/LeapEvent.ts ../modules/util/LeapUtil.ts ../modules/Controller.ts ../modules/Pointable.ts ../modules/Gesture.ts ../modules/Finger.ts ../modules/Tool.ts ../modules/Hand.ts ../modules/Frame.ts ../modules/Matrix.ts ../modules/CircleGesture.ts ../modules/KeyTapGesture.ts ../modules/ScreenTapGesture.ts ../modules/SwipeGesture.ts ../modules/Vector3.ts | sed 's/\/\/\/.*$//' > ../LeapMotionTS_combined.ts
 
@@ -7,7 +7,7 @@ echo "[Making module/interface external...]"
 sed 's/class /export class /' ../LeapMotionTS_combined.ts | sed 's/interface /export interface /' > ../LeapMotionTS.ts
 
 echo "[Running TypeScript compiler...]"
-tsc -b --module amd --target ES5 ../LeapMotionTS.ts
+tsc --declaration -b --module amd --target ES5 ../LeapMotionTS.ts
 
 #echo "[Running RequireJS Optimizer...]"
 #node r.js -o baseUrl=../ name=LeapMotionTS out=../LeapMotionTS_optimized.js
