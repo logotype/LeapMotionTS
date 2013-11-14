02c8a8df7e702e3d8e5505606d17509b73a86c33
Victor Norgren <victor@logotype.se>
2013-11-14 21:15:17 +0800

    init connection bugfix (v4 protocol)

--- build/leapmotionts-1.0.9.ts
+++ build/leapmotionts-1.0.9.ts
@@ -569,9 +569,9 @@ export class Controller extends EventDispatcher
         {
             this._isConnected = true;
             this.listener.onConnect( this );
-            var backgroundData:any = {};
-            backgroundData.background = true;
-            this.connection.send( JSON.stringify( backgroundData ) );
+            var focusedState:any = {};
+            focusedState.focused = true;
+            this.connection.send( JSON.stringify( focusedState ) );
         };
 
         this.connection.onclose = ( data:Object ) =>
--- src/Controller.ts
+++ src/Controller.ts
@@ -108,9 +108,9 @@ class Controller extends EventDispatcher
         {
             this._isConnected = true;
             this.listener.onConnect( this );
-            var backgroundData:any = {};
-            backgroundData.background = true;
-            this.connection.send( JSON.stringify( backgroundData ) );
+            var focusedState:any = {};
+            focusedState.focused = true;
+            this.connection.send( JSON.stringify( focusedState ) );
         };
 
         this.connection.onclose = ( data:Object ) =>
